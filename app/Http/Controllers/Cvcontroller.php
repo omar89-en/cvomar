a7b4b4167cbafb167fbb2eed2973be5a445e3068
soufiane taouil <taouil.soufiane1997@gmail.com>
2021-01-04 23:09:44 +0100

    Fix cv photo.

--- app/Http/Controllers/Cvcontroller.php
+++ app/Http/Controllers/Cvcontroller.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Cv;
 
 use Illuminate\Http\Request;
@@ -11,73 +12,74 @@ use Illuminate\http\UploadedFile;
 class Cvcontroller extends Controller
 {
 
-    public function __construct(){
+    public function __construct()
+    {
 
         $this->middleware('auth');
-     
-         }
-    public function create(){
+    }
+    public function create()
+    {
 
         return view('cv.create');
-
     }
     public function index()
     {
-        $listcv =Auth::user()->cvs;
-        return view('cv.index', ['cvs'=>$listcv ]);
-
+        $listcv = Auth::user()->cvs;
+        return view('cv.index', ['cvs' => $listcv]);
     }
-    public function store(Request $request){
-        $cv= new Cv();
+    public function store(Request $request)
+    {
+        $cv = new Cv();
         $cv->titre = $request->input('titre');
         $cv->presantation = $request->input('presantation');
-         
+
 
         if ($request->hasFile('photo')) {
-            $cv->photo->$request->photo->store('image');
-
+            $cv->photo = $request->photo->store('image');
         }
 
-         $request->validate([
-                'titre' => 'required',
-                'presantation' => 'required|min:5',
-                
-            ]);
-   
+        $request->validate([
+            'titre' => 'required',
+            'presantation' => 'required|min:5',
+
+        ]);
+
         $input = $request->all();
-        $cv->user_id =Auth::user()->id;
+        $cv->user_id = Auth::user()->id;
 
         $cv->save();
         session()->flash('message', 'Post successfully .');
         return redirect('index');
     }
-    public function edit($id){
-        $cv =Cv::find($id);
-         return view('cv.edit', ['cv'=>$cv ]);
-
+    public function edit($id)
+    {
+        $cv = Cv::find($id);
+        return view('cv.edit', ['cv' => $cv]);
     }
-    public function update( Request $request , $id){
-        $cv =Cv::find($id);
+    public function update(Request $request, $id)
+    {
+        $cv = Cv::find($id);
 
-     $cv->titre = $request->input('titre');
-        
+        $cv->titre = $request->input('titre');
+
         $cv->presantation = $request->input('presantation');
-        
-         $request->validate([
-                'titre' => 'required',
-                'presantation' => 'required|min:5',
-                
-            ]);
-   
+
+        $request->validate([
+            'titre' => 'required',
+            'presantation' => 'required|min:5',
+
+        ]);
+
         $input = $request->all();
 
         $cv->save();
         session()->flash('message', 'Post modifie .');
         return redirect('index');
     }
-    public function destroy (Request $request ,$id){
-        $cv =Cv::find($id);
+    public function destroy(Request $request, $id)
+    {
+        $cv = Cv::find($id);
         $cv->delete();
-       return redirect('index');
-   }
+        return redirect('index');
+    }
 }
